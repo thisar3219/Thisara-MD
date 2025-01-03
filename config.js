aa2318791f230d963164b7c91b36a59de58abf7f
thisar3219 <122536722+thisar3219@users.noreply.github.com>
2025-01-03 20:44:41 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,15 +5,15 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "n2piHRbB#BKvC1i-6ewt8pddVV34_HCGvB7mBJ7gNEua30-xAsMI", // Add your session id
 PREFIX: process.env.PREFIX || ".",
-AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "false",
 MODE: process.env.MODE || "public",
-AUTO_VOICE: process.env.AUTO_VOICE || "true",
+AUTO_VOICE: process.env.AUTO_VOICE || "false",
 AUTO_STICKER: process.env.AUTO_STICKER || "false",
 AUTO_REPLY: process.env.AUTO_REPLY || "false",
 AUTO_REACT: process.env.AUTO_REACT || "false",
-FAKE_RECORDING: process.env.FAKE_RECORDING || "true",   
+FAKE_RECORDING: process.env.FAKE_RECORDING || "false",   
 ANTI_LINK: process.env.ANTI_LINK || "true",
 ANTI_BAD: process.env.ANTI_BAD || "true",   
 READ_MESSAGE: process.env.READ_MESSAGE || "false",
